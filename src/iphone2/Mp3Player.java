e1b30ce656840db23491f12f987a242c06054c9b
Kana-Chris <nyapo797@gmail.com>
2024-03-05 23:14:11 +0900

    実装

--- src/iphone2/Mp3Player.java
+++ src/iphone2/Mp3Player.java
@@ -2,10 +2,7 @@ package iphone2;
 
 public interface Mp3Player {
 	public abstract void play();
-	
 	public abstract void stop();
-	
 	public abstract void next();
-	
 	public abstract void back();
 }
